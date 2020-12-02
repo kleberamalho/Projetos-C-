594e10a326180576066afcb25fd685d4e86d1932
Kleber Ramalho <kleber.ramalho96@hotmail.com>
2020-12-01 21:25:14 -0300

    C#

---
+++ ConnectionSql/ConnectionSql/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ConnectionSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTeste = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnTeste
+            // 
+            this.BtnTeste.Location = new System.Drawing.Point(268, 191);
+            this.BtnTeste.Name = "BtnTeste";
+            this.BtnTeste.Size = new System.Drawing.Size(123, 74);
+            this.BtnTeste.TabIndex = 0;
+            this.BtnTeste.Text = "Testar";
+            this.BtnTeste.UseVisualStyleBackColor = true;
+            this.BtnTeste.Click += new System.EventHandler(this.BtnTeste_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 501);
+            this.Controls.Add(this.BtnTeste);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTeste;
+    }
+}
+
---
+++ ConnectionSql/ConnectionSql/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ConnectionSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTeste_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectioString;
+                SqlConnection conexao;
+                connectioString = @"Server = DESKTOP-2A8U9LA\SQLEXPRESS; Database = Clientes;Trusted_Connection=Tru";
+                conexao = new SqlConnection(connectioString);
+                conexao.Open();
+                MessageBox.Show("Connection Open!");
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("Erro ao se conectar no banco de dados \n"+
+                    "Verificar os dados informados" + erro);
+            }
+        }
+    }
+}
